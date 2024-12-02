a58bd2064f1f2cff869095c92f062bdbb984119e
Gracjan Józefczyk <juzkos@gmail.com>
2024-12-02 20:27:31 +0100

    Resources

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace SyliusAcademy\WorkshopPlugin\DependencyInjection;
 
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Sylius\Resource\Factory\Factory;
+use Sylius\Resource\Factory\TranslatableFactory;
+use SyliusAcademy\WorkshopPlugin\Entity\Brand\Brand;
+use SyliusAcademy\WorkshopPlugin\Entity\Brand\BrandInterface;
+use SyliusAcademy\WorkshopPlugin\Entity\Brand\BrandTranslation;
+use SyliusAcademy\WorkshopPlugin\Entity\Brand\BrandTranslationInterface;
+use SyliusAcademy\WorkshopPlugin\Form\Type\BrandTranslationType;
+use SyliusAcademy\WorkshopPlugin\Form\Type\BrandType;
+use SyliusAcademy\WorkshopPlugin\Repository\BrandRepository;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -17,6 +29,60 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('sylius_academy_workshop');
         $rootNode = $treeBuilder->getRootNode();
 
+        $rootNode
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('driver')->defaultValue(SyliusResourceBundle::DRIVER_DOCTRINE_ORM)->end()
+            ->end()
+        ;
+
+        $this->addResourcesSection($rootNode);
+
         return $treeBuilder;
+    }
+
+    private function addResourcesSection(ArrayNodeDefinition $node): void
+    {
+        $node
+            ->children()
+                ->arrayNode('resources')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('brand')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->arrayNode('classes')
+                                    ->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->scalarNode('model')->defaultValue(Brand::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('interface')->defaultValue(BrandInterface::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('repository')->defaultValue(BrandRepository::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('factory')->defaultValue(TranslatableFactory::class)->end()
+                                        ->scalarNode('form')->defaultValue(BrandType::class)->cannotBeEmpty()->end()
+                                    ->end()
+                                ->end()
+                                ->arrayNode('translation')
+                                    ->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->arrayNode('classes')
+                                            ->addDefaultsIfNotSet()
+                                            ->children()
+                                                ->scalarNode('model')->defaultValue(BrandTranslation::class)->cannotBeEmpty()->end()
+                                                ->scalarNode('interface')->defaultValue(BrandTranslationInterface::class)->cannotBeEmpty()->end()
+                                                ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                                ->scalarNode('repository')->cannotBeEmpty()->end()
+                                                ->scalarNode('factory')->defaultValue(Factory::class)->end()
+                                                ->scalarNode('form')->defaultValue(BrandTranslationType::class)->cannotBeEmpty()->end()
+                                            ->end()
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
     }
 }
